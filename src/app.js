1bcb1dbb0752ec5fb3055fda0bb359ef024b94fd
emracr <emrullah.acar444@gmail.com>
2022-10-07 16:22:57 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+let credits = ["Konut Kredisi", "Taşıt Kredisi", "Emlak Kredisi", "Kamu Kredisi", "SGK Kredisi"]
+
+console.log("<ul>")
+
+for(let credit of credits)
+    console.log("<li>" + credit + "</ul>")
+
+console.log("</ul")
